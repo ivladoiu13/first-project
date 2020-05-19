b918bf56a9342c0211815b8ae23577d12c61db9a
ivladoiu13 <65619813+ivladoiu13@users.noreply.github.com>
2020-05-19 20:53:45 +0300

    initials changes

---
+++ escapechar.py
@@ -0,0 +1,27 @@
+splitString = "This string has been \n split over \n several \n lines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4"
+print(tabbedString)
+
+print('The pet shop owener said "No, no, \'e\'s uh, ...he\'s resting"')
+
+exp = """This has been
+ split over 
+ few lines"""
+
+print(exp)
+
+
+x = "Number1 \t The Larch \nNumber2 \t The House Chestnut"
+print(x)
+
+
+lists = [[] for i in range(2)]
+lists[0].append(3)
+lists[1].append(5)
+#lists[2].append(7)
+print(lists)
+
+x = 'Pys' in 'Python'
+print(x)
---
+++ helloworld.py
@@ -0,0 +1,12 @@
+print ("Hello, World!")
+print (1+2)
+print(7 * 6)
+print ()
+print ('The End')
+print("The end", "or is it", "keep watching", 3)
+
+
+x = 2
+y = 4
+print("The sum is: ", x+y)
+
